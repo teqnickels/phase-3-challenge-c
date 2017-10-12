dac6b3b22a7c5bf44f0b6c44e3c55416d4638046
Debrena McEwen <debrenamcewen@gmail.com>
2017-10-12 12:22:21 -0700

    add complete switch case for commands

--- part-2/db/db.js
+++ part-2/db/db.js
@@ -10,47 +10,37 @@ CASE WHEN check_out < DATE(NOW()) THEN 'true' WHEN check_out > DATE(NOW()) THEN 
 END AS available FROM rooms JOIN bookings ON rooms.id = bookings.room_id`
 
 const listRoomsAvailable = `SELECT
-  room_number, 
-  capacity, 
-  'true' as available 
+  room_number, capacity, 'true' as available 
 	FROM rooms 
   JOIN bookings 
 	ON rooms.id = bookings.room_id 
   WHERE check_out < DATE(NOW())`
 
 const upcomingBookings = `SELECT
-	room_number, 
-  name, 
-	check_in, 
-	check_out
-  FROM
-  guests
+	room_number, name, to_char(check_in, 'DD/MM/YYYY') AS check_in,	to_char(check_out, 'DD/MM/YYYY') AS check_out	
+  FROM guests
   INNER JOIN
   bookings ON guests.id = bookings.guest_id
   INNER JOIN
   rooms ON rooms.id = bookings.room_id
-  WHERE check_in > DATE(NOW())
-  OR check_out > DATE(NOW())`
-
+  WHERE check_in > CURRENT_DATE
+  OR check_out > CURRENT_DATE`
 
 const singleRooms = `SELECT
-	room_number, 
-	name, 
-	check_in, 
-	check_out
-  FROM
-  guests
+	room_number, name, to_char(check_in, 'DD/MM/YYYY') AS check_in, to_char(check_out, 'DD/MM/YYYY') AS check_out
+  FROM guests
   INNER JOIN
   bookings ON guests.id = bookings.guest_id
   INNER JOIN
   rooms ON rooms.id = bookings.room_id
-  WHERE room_number = $1 AND (check_in > DATE(NOW()) OR (check_out > DATE(NOW())))`
+  WHERE room_number = $1 AND (check_in > CURRENT_DATE OR (check_out > CURRENT_DATE))`
 
 const queryFunctions = {
   list : () => db.any(listGuests),
   listAll: () => db.any(listAllRooms),
   listAvailable: () => db.any(listRoomsAvailable), 
-  bookings: () => db.any(upcomingBookings)
+  bookings: () => db.any(upcomingBookings),
+  single: (room_number) => db.any(singleRooms, room_number)
 }
 
 module.exports = { queryFunctions, pgp }
