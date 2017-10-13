30b1d77798bdd019c68188d8a5928c1b372c3926
Debrena McEwen <debrenamcewen@gmail.com>
2017-10-12 21:44:42 -0700

    add error handlers to hotel.js

--- part-2/hotel.js
+++ part-2/hotel.js
@@ -1,9 +1,15 @@
 const command = process.argv[2]
-const arg = process.argv.slice([3]).join().replace(/\,/g, ' ')
+let arg = process.argv.slice([3]).join().replace(/\,/g, ' ')
 const { queryFunctions, pgp } = require('./db/db.js')
 require('console.table');
 
 const processCommands = () => {
+
+  console.log(command)
+  if(!command) {
+    throw 'Must Enter Command'
+  }
+
   switch (command) {
     case 'guests':
       return queryFunctions.list()
@@ -11,7 +17,7 @@ const processCommands = () => {
           let outerArr = []
           for(let i = 0; i < value.length; i++) {
             let arr = []
-            arr.push('|  ' + value[i].id, value[i].name, value[i].email)
+            arr.push( value[i].id, value[i].name, value[i].email)
             outerArr.push(arr)
           }
           pgp.end()
@@ -20,12 +26,13 @@ const processCommands = () => {
 
     case 'rooms':
     if(arg) {
+      console.log()
       return queryFunctions.listAvailable()
         .then( (value) => {
           let outerArr = [];
           for (let i = 0; i < value.length; i++) {
             let arr = []
-            arr.push('|  ' + value[i].room_number, value[i].capacity, value[i].available)
+            arr.push(value[i].room_number, value[i].capacity, value[i].available)
             outerArr.push(arr)
           }
           pgp.end()
@@ -37,7 +44,7 @@ const processCommands = () => {
           let outerArr = [];
           for (let i = 0; i < value.length; i++) {
             let arr = []
-            arr.push('|  ' + value[i].room_number, value[i].capacity, value[i].available)
+            arr.push(value[i].room_number, value[i].capacity, value[i].available)
             outerArr.push(arr)
           }
           pgp.end()
@@ -48,12 +55,13 @@ const processCommands = () => {
 
     case 'bookings':
        if(arg) {
+         arg = arg.toUpperCase()
          return queryFunctions.single(arg)
           .then((value) => {
             let outerArr = [];
             for (let i = 0; i < value.length; i++) {
               let arr = []
-              arr.push('|  ' + value[i].room_number, value[i].name, value[i].check_in, value[i].check_out)
+              arr.push(value[i].room_number, value[i].name, value[i].check_in, value[i].check_out)
               outerArr.push(arr)
             }
             pgp.end()
@@ -65,13 +73,16 @@ const processCommands = () => {
              let outerArr = [];
              for (let i = 0; i < value.length; i++) {
                let arr = []
-               arr.push('|  ' + value[i].room_number, value[i].name, value[i].check_in, value[i].check_out)
+               arr.push(value[i].room_number, value[i].name, value[i].check_in, value[i].check_out)
                outerArr.push(arr)
              }
              pgp.end()
              console.table(['Room Number', 'Guest Name', 'Check-In', 'Check-Out'], outerArr)
            })
        }
+
+       default:
+         throw 'Command must be one of the following: guests, rooms, bookings'
   }
 }
 
