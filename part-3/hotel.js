5dd96757b53785b4346d974510471a1cf25cd47f
Debrena McEwen <debrenamcewen@gmail.com>
2017-10-13 10:39:45 -0700

    Add click handler for all buttons

--- part-3/hotel.js
+++ part-3/hotel.js
@@ -1,54 +1,51 @@
 window.addEventListener('load', () => {
 
-  const button1 = document.getElementsByClassName("button1")[0]
-  // const button2 = document.getElementsByClassName("button2")[1]
-  // const button3 = document.getElementsByClassName("button3")[2]
-  // const button4 = document.getElementsByClassName("button4")[3]
-  // const button5 = document.getElementsByClassName("button5")[4]
-  // const button6 = document.getElementsByClassName("button6")[5]
-  // const button7 = document.getElementsByClassName("button7")[6]
+  const button1 = document.getElementsByTagName("button")[1]
+  const button2 = document.getElementsByTagName("button")[2]
+  const button3 = document.getElementsByTagName("button")[3]
+  const button4 = document.getElementsByTagName("button")[4]
+  const button5 = document.getElementsByTagName("button")[5]
+  const button6 = document.getElementsByTagName("button")[6]
+  const button7 = document.getElementsByTagName("button")[7]
 
-    button1.addEventListener('click', () => {
-     buildModal('room2B')
-    });
+  const buildModal = (button) => {
+    let modal = document.getElementsByClassName("modal-hidden")[0]
+    modal.className = "modal-display";
 
-    // button2.addEventListener('click', () => {
-    //   const room2A = document.getElementById(room2A)
-    //   buildModal(room2A)
-    // });
+      switch (button) {
+        case button1:
+          console.log("BUTTON 1 CLICKED!")
+          break;
 
-    // button3.addEventListener('click', () => {
-    //   const room2B = document.getElementById(room2B)
-    //   buildModal(room2B)
-    // });
+        case button2:
+          console.log("BUTTON 2 CLICKED!")
+          break;
 
-    // button4.addEventListener('click', () => {
-    //   const room3A = document.getElementById(room3A)
-    //   buildModal(room3A)
-    // });
+        case button3:
+          console.log("BUTTON 3 CLICKED!")
+          break;
 
-    // button5.addEventListener('click', () => {
-    //   const room3B = document.getElementById(room3B)
-    //   buildModal(room3B)
-    // });
+        case button4:
+          console.log("BUTTON 3 CLICKED!")
+          break;
 
-    // button6.addEventListener('click', () => {
-    //   const room3C = document.getElementById(room3C)
-    //   buildModal(room3C)
-    // });
+        case button5:
+          console.log("BUTTON 3 CLICKED!")
+          break;
 
-    // button7.addEventListener('click', () => {
-    //   const room4A = document.getElementById(room4A)
-    //   buildModal(room4A)
-    // });
-
-    const buildModal = (room) => {
-      let modal = document.getElementsByClassName("modal-hidden")[0]
-      modal.className = "modal-display";
-
+        case button6:
+          console.log("BUTTON 3 CLICKED!")
+          break;
+          
+        case button7:
+          console.log("BUTTON 3 CLICKED!")
+          break;
+      }
     }
 
+  var buttons = document.querySelectorAll("button")
+    console.log(buttons)
+  for (let button of buttons) {
+    button.addEventListener('click', buildModal.bind(this, button))
+  }
 })
-
-
-
