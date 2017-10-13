d326830d2b55537a15d6ea5cb16690f936d6727e
Debrena McEwen <debrenamcewen@gmail.com>
2017-10-13 10:52:37 -0700

    Add click handlers

--- part-3/hotel.js
+++ part-3/hotel.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
 
+  const closeModalButton = document.getElementsByClassName("modal-content-close-button")[0]
   const button1 = document.getElementsByTagName("button")[1]
   const button2 = document.getElementsByTagName("button")[2]
   const button3 = document.getElementsByTagName("button")[3]
@@ -7,9 +8,9 @@ window.addEventListener('load', () => {
   const button5 = document.getElementsByTagName("button")[5]
   const button6 = document.getElementsByTagName("button")[6]
   const button7 = document.getElementsByTagName("button")[7]
+  let modal = document.getElementsByClassName("modal-hidden")[0]
 
-  const buildModal = (button) => {
-    let modal = document.getElementsByClassName("modal-hidden")[0]
+  const openModal = (button) => {
     modal.className = "modal-display";
 
       switch (button) {
@@ -26,26 +27,35 @@ window.addEventListener('load', () => {
           break;
 
         case button4:
-          console.log("BUTTON 3 CLICKED!")
+          console.log("BUTTON 4 CLICKED!")
           break;
 
         case button5:
-          console.log("BUTTON 3 CLICKED!")
+          console.log("BUTTON 5 CLICKED!")
           break;
 
         case button6:
-          console.log("BUTTON 3 CLICKED!")
+          console.log("BUTTON 6 CLICKED!")
           break;
-          
+
         case button7:
-          console.log("BUTTON 3 CLICKED!")
+          console.log("BUTTON 7 CLICKED!")
+          break;
+        
+          case closeModalButton:
+          closeModal()
           break;
       }
     }
 
+    const closeModal = () => {
+      modal.className = "modal-hidden"
+    }
+
+
   var buttons = document.querySelectorAll("button")
     console.log(buttons)
   for (let button of buttons) {
-    button.addEventListener('click', buildModal.bind(this, button))
+    button.addEventListener('click', openModal.bind(this, button))
   }
 })
